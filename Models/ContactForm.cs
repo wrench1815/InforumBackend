330f37e3430d9fca39e1c446a5cd99b4c704990b
Hardeep Kumar <hardeepkumar1815@gmail.com>
2021-11-21 23:12:03 +0530

    Add project files.

---
+++ Models/ContactForm.cs
@@ -0,0 +1,17 @@
+﻿namespace InforumBackend.Models {
+	public class ContactForm {
+		public long Id { get; set; }
+
+		public string? FullName { get; set; }
+
+		public string? Email { get; set; }
+
+		public string? Message { get; set; }
+
+		public DateTime CreatedOn { get; set; }
+
+		public ContactForm() {
+			CreatedOn = DateTime.Now;
+		}
+	}
+}
---
+++ Models/Home.cs
@@ -0,0 +1,11 @@
+﻿namespace InforumBackend.Models {
+	public class Home {
+		public long Id { get; set; }
+
+		public string? Heading { get; set; }
+
+		public string? SubHeading { get; set; }
+
+		public string? HeaderImageLink { get; set; }
+	}
+}
