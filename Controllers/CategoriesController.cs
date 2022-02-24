e6dff01cc85c53f5fe3b3f1b48c56dceb708d081
Hardeep Kumar <hardeepkumar1815@gmail.com>
2022-02-24 12:18:20 +0530

    Update CategoriesController
    Add Logger to CategoriesController
    Update CategoriesController to prevent posts and Queries not having Category if deleted
    Update logic to prevent deletion of General Category

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -4,6 +4,7 @@ using InforumBackend.Data;
 using InforumBackend.Models;
 using Microsoft.AspNetCore.Authorization;
 using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace InforumBackend.Controllers
 {
@@ -13,37 +14,58 @@ namespace InforumBackend.Controllers
     {
         private readonly InforumBackendContext _context;
 
-        public CategoriesController(InforumBackendContext context)
+        private readonly ILogger _logger;
+
+        public CategoriesController(InforumBackendContext context, ILoggerFactory logger)
         {
             _context = context;
+            _logger = logger.CreateLogger("CategoriesController");
         }
 
         // GET: api/Categories
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Category>>> GetCategory()
         {
-            var categories = await _context.Category.ToListAsync();
+            try
+            {
+                var categories = await _context.Category.ToListAsync();
 
-            return Ok(categories);
+                return Ok(categories);
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return BadRequest();
+            }
+
         }
 
         // GET: api/Categories/5
         [HttpGet("{id}")]
         public async Task<ActionResult<Category>> GetCategory(long id)
         {
-            var category = await _context.Category.FindAsync(id);
-
-            if (category == null)
+            try
             {
-                return NotFound(new
-                {
-                    Status = StatusCodes.Status404NotFound,
-                    Message = "Category not Found."
-                }
-                );
-            }
+                var category = await _context.Category.FindAsync(id);
 
-            return Ok(category);
+                if (category == null)
+                {
+                    _logger.LogInformation("Category of id: {0} not found.", id);
+                    return NotFound(new
+                    {
+                        Status = StatusCodes.Status404NotFound,
+                        Message = "Category not Found."
+                    }
+                    );
+                }
+
+                return Ok(category);
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError(ex.ToString());
+                return BadRequest();
+            }
         }
 
         // GET: api/Categories/slug/5
@@ -56,6 +78,7 @@ namespace InforumBackend.Controllers
 
                 if (category == null)
                 {
+                    _logger.LogInformation("Category of slug: {0} not found.", slug);
                     return NotFound(new
                     {
                         Status = StatusCodes.Status404NotFound,
@@ -65,8 +88,9 @@ namespace InforumBackend.Controllers
 
                 return Ok(category);
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
+                _logger.LogError(ex.ToString());
                 return BadRequest();
             }
         }
@@ -77,33 +101,52 @@ namespace InforumBackend.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> PutCategory(long id, Category category)
         {
-            if (id != category.Id)
-            {
-                return BadRequest(new
-                {
-                    Status = StatusCodes.Status400BadRequest,
-                    Message = "Check if request data is Correct."
-                });
-            }
-
-            var cat_slug = generateSlug(category.Name);
-            category.Slug = cat_slug;
-
-            _context.Entry(category).State = EntityState.Modified;
-
             try
             {
+                if (id != category.Id)
+                {
+                    _logger.LogInformation("Category of id: {0} not found.", id);
+                    return BadRequest(new
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Message = "Check if request data is Correct."
+                    });
+                }
+
+                // check if category with name exists
+                var categoryName = await _context.Category.FirstOrDefaultAsync(i => i.Name == toTitleCase(category.Name));
+
+                if (categoryName != null)
+                {
+                    _logger.LogInformation("Category with name: {0} already exists.", category.Name);
+                    return BadRequest(new
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Message = "Category with name: " + category.Name + " already exists."
+                    });
+                }
+
+                // get slug for category and Titlecase Name
+                var cat_slug = generateSlug(category.Name);
+                category.Slug = cat_slug;
+                category.Name = toTitleCase(category.Name);
+
+                _context.Entry(category).State = EntityState.Modified;
+
                 await _context.SaveChangesAsync();
+                _logger.LogInformation("Category of id: {0} updated.", id);
+
                 return Ok(new
                 {
                     Status = StatusCodes.Status200OK,
                     Message = "Category Modified Successfully."
                 });
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
                 if (!CategoryExists(id))
                 {
+                    _logger.LogInformation("Category of id: {0} not found.", id);
                     return NotFound(new
                     {
                         Status = StatusCodes.Status404NotFound,
@@ -112,6 +155,7 @@ namespace InforumBackend.Controllers
                 }
                 else
                 {
+                    _logger.LogError(ex.ToString());
                     return BadRequest();
                 }
             }
@@ -125,11 +169,12 @@ namespace InforumBackend.Controllers
         {
             try
             {
-                var cat_name = category.Name;
+                var cat_name = toTitleCase(category.Name);
                 var cat_exist = await _context.Category.AnyAsync(i => i.Name == cat_name);
 
                 if (cat_exist)
                 {
+                    _logger.LogInformation("Category of name: {0} already exist.", cat_name);
                     return BadRequest(new
                     {
                         Status = StatusCodes.Status400BadRequest,
@@ -140,9 +185,12 @@ namespace InforumBackend.Controllers
                 var cat_slug = generateSlug(category.Name);
 
                 category.Slug = cat_slug;
+                category.Name = toTitleCase(category.Name);
 
                 _context.Category.Add(category);
+
                 await _context.SaveChangesAsync();
+                _logger.LogInformation("Category of name: {0} created.", cat_name);
 
                 return StatusCode(StatusCodes.Status201Created, new
                 {
@@ -150,8 +198,9 @@ namespace InforumBackend.Controllers
                     Message = "Category created Successfully.",
                 });
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
+                _logger.LogError(ex.ToString());
                 return BadRequest();
             }
         }
@@ -161,20 +210,72 @@ namespace InforumBackend.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCategory(long id)
         {
-            var category = await _context.Category.FindAsync(id);
-            if (category == null)
-            {
-                return StatusCode(StatusCodes.Status404NotFound, new
-                {
-                    Status = StatusCodes.Status200OK,
-                    Message = "Category does not exist."
-                });
-            }
-
             try
             {
+                var category = await _context.Category.FindAsync(id);
+
+                var delCatname = toTitleCase(category.Name);
+
+                if (category == null)
+                {
+                    _logger.LogInformation("Category of id: {0} not found.", id);
+                    return StatusCode(StatusCodes.Status404NotFound, new
+                    {
+                        Status = StatusCodes.Status404NotFound,
+                        Message = "Category does not exist."
+                    });
+                }
+
+                if (category.Name == "General")
+                {
+                    _logger.LogInformation("Cannot delete General Category.");
+                    return BadRequest(new
+                    {
+                        Status = StatusCodes.Status400BadRequest,
+                        Message = "Cannot delete General Category."
+                    });
+                }
+
+                // Search category with name General
+                var cat_general = await _context.Category.FirstOrDefaultAsync(i => i.Name == "General");
+
+                // find all BlogPosts with this category and change their category with default category
+                var blogPosts = await _context.BlogPost.Where(i => i.CategoryId == id).ToListAsync();
+
+                if (blogPosts.Count > 0)
+                {
+                    foreach (var blogPost in blogPosts)
+                    {
+                        blogPost.CategoryId = cat_general.Id;
+
+                        _logger.LogInformation("Category of BlogPost id: {0} changed to General.", blogPost.Id);
+                    }
+
+                    _context.BlogPost.UpdateRange(blogPosts);
+                    await _context.SaveChangesAsync();
+                    _logger.LogInformation("Saved changes to BlogPosts.");
+                }
+
+                // find all ForumQueries with this category and change their category with default category
+                var forumQueries = await _context.ForumQuery.Where(i => i.CategoryId == id).ToListAsync();
+
+                if (forumQueries.Count > 0)
+                {
+                    foreach (var forumQuery in forumQueries)
+                    {
+                        forumQuery.CategoryId = cat_general.Id;
+
+                        _logger.LogInformation("Category of ForumQuery id: {0} changed to General.", forumQuery.Id);
+                    }
+
+                    _context.ForumQuery.UpdateRange(forumQueries);
+                    await _context.SaveChangesAsync();
+                    _logger.LogInformation("Saved changes to ForumQueries.");
+                }
+
                 _context.Category.Remove(category);
                 await _context.SaveChangesAsync();
+                _logger.LogInformation("Category of id: {0} deleted.", id);
 
                 return Ok(new
                 {
@@ -182,8 +283,9 @@ namespace InforumBackend.Controllers
                     Message = "Category Deleted Successfully."
                 });
             }
-            catch (System.Exception)
+            catch (System.Exception ex)
             {
+                _logger.LogError(ex.ToString());
                 return BadRequest();
             }
         }
@@ -205,7 +307,15 @@ namespace InforumBackend.Controllers
             // replace spaces with dashes(-)
             slug = Regex.Replace(slug, @"\s", "-");
 
+            _logger.LogInformation("Slug generated: {0}", slug);
+
             return slug;
+        }
+
+        // to titlecase
+        private string toTitleCase(string str)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(str.ToLower());
         }
     }
 }
